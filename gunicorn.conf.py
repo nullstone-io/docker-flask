c3f678faac40134b60de3f3a92809d3ef5bf4ee5
Brad Sickles <brad.sickles@gmail.com>
2022-05-19 15:34:02 -0400

    Fix PORT and logging in gunicorn conf

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,6 +4,12 @@ import multiprocessing
 import os
 
 wsgi_app = os.getenv('WSGI_APP', 'app:app')
-bind = os.getenv('BINDING', '0.0.0.0:80')
+port = os.getenv('PORT', '80')
+bind = os.getenv('BINDING', f'0.0.0.0:{port}')
 workers = int(os.getenv('WEB_CONCURRENCY', multiprocessing.cpu_count() * 2))
 threads = int(os.getenv('PYTHON_MAX_THREADS', 1))
+
+# Send access and error logs to stdout
+accesslog = '-'
+errorlog = '-'
+capture_output = False
